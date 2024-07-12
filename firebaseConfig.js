b9a2570d3c80d6ed8fdebe421ea5ef6dbdf9df1c
Shivansh Shukla <prasantshukla89@gmail.com>
2024-07-12 18:58:12 +0530

    making final change

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase, ref, set } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+ const firebaseConfig = {
   apiKey: "AIzaSyD7JbfTHm2qFvnpCe8PDHE42vcrujYbysU",
   authDomain: "ciie-summer-training-program.firebaseapp.com",
   projectId: "ciie-summer-training-program",
@@ -15,8 +13,7 @@ const firebaseConfig = {
   appId: "1:230709441202:web:84017542175ed5ffbe4fab",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const firestore = getFirestore(app);
