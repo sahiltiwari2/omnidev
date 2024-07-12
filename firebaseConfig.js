6302a9aa1e158f1c85deb8e9c38c97e803308905
Shivansh Shukla <prasantshukla89@gmail.com>
2024-07-12 22:09:20 +0530

    added

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getDatabase, ref, set } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
  const firebaseConfig = {
   apiKey: "AIzaSyD7JbfTHm2qFvnpCe8PDHE42vcrujYbysU",
   authDomain: "ciie-summer-training-program.firebaseapp.com",
@@ -12,11 +11,9 @@ import { getStorage } from "firebase/storage";
   messagingSenderId: "230709441202",
   appId: "1:230709441202:web:84017542175ed5ffbe4fab",
 };
-
- const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
-
 export { auth, database, firestore, storage, ref, app};
